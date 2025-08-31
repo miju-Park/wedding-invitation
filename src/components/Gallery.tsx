2d5edad748bcbc5107d0d331e7e2a2214f823a2d
miju-Park <gi6238@naver.com>
2025-08-31 16:08:42 +0900

    update

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,18 +1,33 @@
-import { useRef, useState } from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import { ImageViewer } from './ImageViewer';
+import { useCallback, useMemo, useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
-const images = Array.from({ length: 12 }, (_, i) => `/images/img${i}.webp`);
+const Image = ({ src, goSlide }: { src: string; goSlide: () => void }) => {
+  return (
+    <li className='relative w-full after:block after:content:"" after:pb-[100%]'>
+      <img
+        src={src}
+        onClick={() => goSlide()}
+        alt="갤러리이미지"
+        className="w-full absolute h-full left-0 object-cover rounded-lg  cursor-pointer hover:scale-105"
+      />
+    </li>
+  );
+};
 
 function Gallery() {
   const swiperRef = useRef<any>(null);
 
-  const goToSlide = (index: number) => {
+  const images = useMemo(
+    () => Array.from({ length: 12 }, (_, i) => `/images/img${i}.webp`),
+    []
+  );
+
+  const goToSlide = useCallback((index: number) => {
     if (swiperRef.current) {
       swiperRef.current.slideTo(index);
     }
-  };
+  }, []);
 
   return (
     <div>
@@ -24,21 +39,23 @@ function Gallery() {
       >
         {images.map((src, idx) => (
           <SwiperSlide key={idx}>
-            <img src={src} alt={`slide-${idx}`} className="w-full h-[400px] object-contain mx-auto rounded" />
+            <img
+              src={src}
+              alt={`slide-${idx}`}
+              className="w-full h-[400px] object-contain mx-auto rounded"
+            />
           </SwiperSlide>
         ))}
       </Swiper>
 
       {/* 썸네일 그리드 */}
-      <ul className='grid grid-cols-3 gap-1.5'>
-        {images.map((src,idx)=> (
-          <li key={idx} className='relative w-full after:block after:content:"" after:pb-[100%]'>
-            <img src={src}      onClick={() => goToSlide(idx)} alt="갤러리이미지" className='w-full absolute h-full left-0 object-cover rounded-lg  cursor-pointer hover:scale-105'/>
-          </li>
+      <ul className="grid grid-cols-3 gap-1.5">
+        {images.map((src, idx) => (
+          <Image src={src} key={idx} goSlide={() => goToSlide(idx)} />
         ))}
       </ul>
-
-  </div>)
+    </div>
+  );
 }
 
 export default Gallery;
