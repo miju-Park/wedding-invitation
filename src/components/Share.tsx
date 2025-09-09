0db24d38d58df211d184ebbfb3d4678dbe98631d
miju-Park <gi6238@naver.com>
2025-09-09 23:18:35 +0900

    feat: 공유하기

--- src/components/Share.tsx
+++ src/components/Share.tsx
@@ -1,33 +1,45 @@
-import { Share2 } from "lucide-react";
-import { useEffect } from "react";
+import { Link } from "lucide-react";
+import { useCallback, useEffect } from "react";
+import { toast } from "sonner";
 
-export const Share = ()=>{
-  useEffect(()=>{
-    useEffect(() => {
-      const script = document.createElement('script')
-      script.src = "https://t1.kakaocdn.net/kakao_js_sdk/2.7.8/kakao.min.js"
-      script.async = true
-  
-      document.head.appendChild(script)
-  
-      script.onload = () => {
-        if (!window.Kakao.isInitialized()) {
-          window.Kakao.init(import.meta.env.PUBLIC_KAKAO_JS_SDK_KEY)
-        }
-      }
-    }, [])
-  },[])
-  return     <div className="flex items-center gap-2 justify-center mx-auto">
-  <div
-    className="cursor-pointer"
-    id="kakaotalk-share-btn"
-  >
-    <img
-      className="size-8"
-      src="https://developers.kakao.com/assets/img/about/logos/kakaotalksharing/kakaotalk_sharing_btn_medium.png"
-      alt="카카오톡 공유 보내기 버튼"
-    />
-  </div>
-  <Share2 />
-</div>
+declare global {
+  interface Window {
+    Kakao: any;
+  }
 }
+
+export const Share = () => {
+  const handleClickKakaoShare = useCallback(() => {
+    const { Kakao } = window;
+    Kakao.Share.sendCustom({
+      templateId: 124100,
+    });
+  }, []);
+
+  const handleCopy = async () => {
+    try {
+      const copyText = window.location.href;
+      await navigator.clipboard.writeText(copyText);
+      toast.success("클립보드에 복사되었습니다!");
+    } catch (err) {
+      toast.error("복사에 실패했습니다.");
+      console.error(err);
+    }
+  };
+  return (
+    <div className="flex items-center gap-2 justify-center mx-auto">
+      <div
+        className="cursor-pointer"
+        id="kakaotalk-share-btn"
+        onClick={handleClickKakaoShare}
+      >
+        <img
+          className="size-8"
+          src="https://developers.kakao.com/assets/img/about/logos/kakaotalksharing/kakaotalk_sharing_btn_medium.png"
+          alt="카카오톡 공유 보내기 버튼"
+        />
+      </div>
+      <Link onClick={handleCopy} className="cursor-pointer" />
+    </div>
+  );
+};
